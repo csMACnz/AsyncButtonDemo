019eda89ee4a16ce8df7ef1a0cf9aa8a29dff857
Mark Clearwater <csmacnz@csmac.co.nz>
2015-05-24 21:04:32 +1200

    Renamed handlers for better representation

--- AsyncButtonDemo/AsyncButtonDemo/MainWindow.xaml.cs
+++ AsyncButtonDemo/AsyncButtonDemo/MainWindow.xaml.cs
@@ -1,18 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace AsyncButtonDemo
 {
@@ -26,12 +15,12 @@ namespace AsyncButtonDemo
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void BadButtonOnClick(object sender, RoutedEventArgs e)
         {
             BuggyCode();
         }
 
-        private async void Button_Click_1(object sender, RoutedEventArgs e)
+        private async void CorrectButtonOnClick(object sender, RoutedEventArgs e)
         {
             await BuggyCode();
         }
